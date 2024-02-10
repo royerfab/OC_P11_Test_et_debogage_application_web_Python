882de690e2717ea887dba3e0dcd49016a39855fc
Fabien Royer <fabroyer.info@gmail.com>
2024-02-10 12:44:07 +0100

    UI change number points available

--- server.py
+++ server.py
@@ -39,7 +39,9 @@ def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        max_booking_places = int(foundClub['points'])
+        return render_template('booking.html',club=foundClub,competition=foundCompetition,
+                               max_booking_places=max_booking_places)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
